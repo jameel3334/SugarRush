b5b43777bd48620336a4cd15ecd64ad114100334
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-09-07 15:39:34 -0400

    Initial Commit

---
+++ SugarRush/SugarRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugarRushApp.swift
+//  SugarRush
+//
+//  Created by Mohammed Jameeluddin on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SugarRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SugarRushUITests/SugarRushUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SugarRushUITestsLaunchTests.swift
+//  SugarRushUITests
+//
+//  Created by Mohammed Jameeluddin on 9/7/23.
+//
+
+import XCTest
+
+final class SugarRushUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
