57f259750f539bb6a4794020affb07ea8a2940a1
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-09-07 18:04:58 -0400

    Model Setup

--- SugarRush/Networking/APIService.swift
+++ SugarRush/Networking/APIService.swift
@@ -15,7 +15,6 @@ struct APIService: APIServiceProtocol {
         let request = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: request)
         guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
-            let statusCode = (response as! HTTPURLResponse).statusCode
             throw APIError.invalidResponse
         }
         let fetchedData:T = try JSONDecoder().decode(T.self, from: data)
